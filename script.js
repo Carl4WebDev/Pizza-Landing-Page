ec9f587460fdf7c2e7dc0732ecaf266d3ec308c8
carl Ivan Sampan <carlforwebdev@gmail.com>
2023-06-14 09:47:24 +0600

    Connect menu and index

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $('.navbar-toggle').on('click', function(){
     $('.navbar-links-mobile').toggle()
 })
+
+$('.navbar button').on('click', function(){
+    $('.navbar a').attr("http", "menu.html")
+})
